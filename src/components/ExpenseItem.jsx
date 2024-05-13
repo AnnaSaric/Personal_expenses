4fd530087491e364b270781c87746c09c8fc94a9
saric.ana456@gmail.com <saric.ana456@gmail.com>
2024-05-13 13:26:10 +0200

    Added ExpenseDate component and CSS for it

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,12 +1,13 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
   return (
     <div className="expense-item">
-      <div>March 28th 2024</div>
+      <ExpenseDate />
       <div className="expense-item__description">
         <h2>New TV</h2>
-        <div className="expense-item__price">400.50$</div>
+        <div className="expense-item__price">€400.50</div>
       </div>
     </div>
   );
